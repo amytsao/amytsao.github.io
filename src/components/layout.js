71a422217ef20fdb7d3b1441637f64b7aa2c0cbe
Amy Tsao <atsao@csod.com>
2019-09-13 19:52:53 -0700

    refactor

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,57 +1,12 @@
-import React, { useState, useCallback } from "react";
-import { useTransition, interpolate } from "react-spring";
-import { Helmet } from "react-helmet";
+import React from "react";
 import "../assets/scss/main.scss";
 
 import Footer from "./Footer";
-import Card from "./Card";
-import Pager from "./Pager";
 
-const Template = props => {
-  const [idx, setIndex] = useState(0);
-  // const items = [{}, {}, {}];
-  const onClick = useCallback(() => {
-    setIndex(i => (i + 1) % 3);
-  }, [setIndex]);
-  const transitions = useTransition(idx, i => i, {
-    initial: null,
-    from: { translate: 1000, rotate: 45 },
-    enter: { translate: 0, rotate: 0 },
-    leave: { translate: -1000, rotate: -45 }
-  });
-
-  const cards = [
-    { title: "Hi!", component: "About" },
-    { title: "About Me", component: "AboutMe" },
-    { title: "Recent Projects", component: "Gallery" }
-  ];
-
+const Template = ({ children }) => {
   return (
     <div className="page-container">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Amy Tsao</title>
-      </Helmet>
-      <div className="pager">
-        <Pager count={3} selected={idx} onSelect={setIndex} cards={cards} />
-      </div>
-      <div className="card-container" onClick={onClick}>
-        {transitions.map(({ item, props, key }) => (
-          <Card
-            key={key}
-            title={cards[item].title}
-            style={{
-              transform: interpolate(
-                [props.translate, props.rotate],
-                (t, rot) =>
-                  `translate3d(${t}px,${t}px,${t}px) rotate(${rot}deg)`
-              )
-            }}
-          >
-            {cards[item].component}
-          </Card>
-        ))}
-      </div>
+      {children}
       <Footer />
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,57 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import { useTransition, interpolate } from "react-spring";
 import Helmet from "react-helmet";
 
+import Card from "../components/Card";
+import Pager from "../components/Pager";
 import Layout from "../components/layout";
 
-class HomeIndex extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <Helmet>
-          <title>Amy Tsao</title>
-          <meta name="description" content="Web Developer" />
-        </Helmet>
-      </Layout>
-    );
-  }
-}
+const HomeIndex = props => {
+  const [idx, setIndex] = useState(0);
+  // const items = [{}, {}, {}];
+  const onClick = useCallback(() => {
+    setIndex(i => (i + 1) % 3);
+  }, [setIndex]);
+  const transitions = useTransition(idx, i => i, {
+    initial: null,
+    from: { translate: 1000, rotate: 45 },
+    enter: { translate: 0, rotate: 0 },
+    leave: { translate: -1000, rotate: -45 }
+  });
+
+  const cards = [
+    { title: "Hi!", component: "About" },
+    { title: "About Me", component: "AboutMe" },
+    { title: "Recent Projects", component: "Gallery" }
+  ];
+  return (
+    <Layout>
+      <Helmet>
+        <title>Amy Tsao</title>
+        <meta name="description" content="Web Developer" />
+      </Helmet>
+      <div className="pager">
+        <Pager count={3} selected={idx} onSelect={setIndex} cards={cards} />
+      </div>
+      <div className="card-container" onClick={onClick}>
+        {transitions.map(({ item, props, key }) => (
+          <Card
+            key={key}
+            title={cards[item].title}
+            style={{
+              transform: interpolate(
+                [props.translate, props.rotate],
+                (t, rot) =>
+                  `translate3d(${t}px,${t}px,${t}px) rotate(${rot}deg)`
+              )
+            }}
+          >
+            {cards[item].component}
+          </Card>
+        ))}
+      </div>
+    </Layout>
+  );
+};
 
 export default HomeIndex;
